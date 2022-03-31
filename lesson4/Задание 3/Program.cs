5f21491845acd99d2ac9fe56e0aa2e1111c7d56a
Evgenii <nechaev2.evgenii.43@gmail.com>
2022-03-31 13:37:39 +0500

    Решение задания 3

--- lesson4/Задание 3/Program.cs
+++ lesson4/Задание 3/Program.cs
@@ -8,8 +8,65 @@ namespace lesson4
 {
     class Program
     {
+        public enum Season
+        {
+            Winter, Spring, Summer, Autumn
+        }
+        public static Season Month(int a)
+        {
+            Season season = new Season();
+            if (a >= 1 && a <= 3)
+                season = Season.Winter;
+            else if (a >= 4 && a <= 6)
+                season = Season.Spring;
+            else if (a >= 7 && a <= 9)
+                season = Season.Summer;
+            else if (a >= 10 && a <= 12)
+                season = Season.Autumn;
+            return season;
+        }
+        public static string NameSeasonOfYear(Season season)
+        {
+            string s="";
+            switch (season)
+            {
+                case (Season.Winter):
+                    s = "Зима";
+                    break;
+                case (Season.Spring):
+                    s = "Весна";
+                    break;
+                case (Season.Summer):
+                    s = "Лето";
+                    break;
+                case (Season.Autumn):
+                    s = "Осень";
+                    break;
+            }
+            return $"Название времени года: {s}";
+        }
         static void Main(string[] args)
         {
+            int i=0;
+            Console.Write("Введите число от 1 до 12: ");
+            try
+            {
+                i = int.Parse(Console.ReadLine());
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Введено не целое чилсо или символ!");
+                Console.ReadLine();
+                return;
+            }
+            if (i > 12 && i < 1)
+            {
+                Console.WriteLine("Необходимо вводить число из промежутка 1 до 12!");
+                Console.ReadLine();
+                return;
+            }
+            Console.WriteLine(NameSeasonOfYear(Month(i)));
+            Console.ReadLine();
         }
     }
 }
