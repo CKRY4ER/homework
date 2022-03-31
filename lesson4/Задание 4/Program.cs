f85ff0f3be0b9485c3bfe5d275a2587e591d8d6a
Evgenii <nechaev2.evgenii.43@gmail.com>
2022-03-31 13:48:28 +0500

    Решение задачи 4

--- lesson4/Задание 4/Program.cs
+++ lesson4/Задание 4/Program.cs
@@ -8,8 +8,17 @@ namespace lesson4
 {
     class Program
     {
+        static int FibRec(int p1, int p2, int n)
+        {
+            return n == 0 ? p1 : FibRec(p2, p1 + p2, n - 1);
+        }
+        static int Fib(int n) { return FibRec(0, 1, n); }
         static void Main(string[] args)
         {
+            Console.Write("Введите число: ");
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Число Фибоначи от числа {i}: {Fib(i)}");
+            Console.ReadLine();
         }
     }
 }
