f78844fc7a39ea909294f9fe5aa631cdc7c45c19
EvgeniiNechaev <e.nechaev@w1w1.money>
2022-04-08 13:27:59 +0500

    Решение задачи 4

--- Lesson5/ConsoleApp1/Program.cs
+++ Lesson5/ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Lesson5
 {
@@ -10,6 +11,18 @@ namespace Lesson5
     {
         static void Main(string[] args)
         {
+            Console.Write("Введите путь: ");
+            string dir = Console.ReadLine();
+            if (!Directory.Exists(dir))
+            {
+                Console.WriteLine("Данной дириектории не существует");
+                Console.ReadKey();
+                return;
+            }
+            string[] allFileInDir = Directory.GetFileSystemEntries(dir);
+            File.WriteAllLines("dir.txt", allFileInDir);
+            Console.WriteLine("Успешно записанно в файл!");
+            Console.ReadKey();
         }
     }
 }
